7ccc4daebe6b13e3087afb7bea804372611a1e3d
Abdulrahman-Mohamad <abd.rahman.mohamad.93@gmail.com>
2025-11-13 23:01:34 +0300

    fixed the error and finished the hero section and starting in animated counter section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,17 @@
 
 export default function Button({ text, className, id }) {
     return <>
-        <a className={`${className ?? ''} cta-wrapper`}>
+        <a
+            onClick={(e) => {
+                e.preventDefault();
+                const target = document.getElementById('counter')
+                if (target && id) {
+                    const offset = window.innerHeight * 0.15;
+                    const top = target.getBoundingClientRect().top + window.scrollY - offset;
+                    window.scrollTo({ top, behavior: "smooth" })
+                }
+            }}
+            className={`${className ?? ''} cta-wrapper`}>
             <div className="cta-button group">
                 <div className="bg-circle" />
                 <p className="text me-5 md:me-0">{text}</p>
@@ -9,6 +19,6 @@ export default function Button({ text, className, id }) {
                     <img src="/images/arrow-down.svg" alt="arrow" />
                 </div>
             </div>
-        </a>
+        </a >
     </>
 }
