bbe8331e1a5c3fe49340e947a47efca5dbad171c
Abdulrahman-Mohamad <abd.rahman.mohamad.93@gmail.com>
2025-11-15 05:38:57 +0300

    finish whole Experience section and starting in TechStack.jsx

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -1,10 +1,60 @@
 import GlowCard from "../components/GlowCard";
 import TitleHeader from "../components/TitleHeader";
 import { expCards } from "../constants";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function ExperienceSection() {
+
+    useGSAP(() => {
+        gsap.utils.toArray(".timeline-card").forEach((card) => {
+            gsap.from(card, {
+                xPercent: -100,
+                opacity: 0,
+                transformOrigin: "left left",
+                duration: 1,
+                ease: "power2.inOut",
+                scrollTrigger: {
+                    trigger: card,
+                    start: "top 80%",
+                },
+            });
+        });
+
+        gsap.to(".timeline", {
+            transformOrigin: "bottom bottom",
+            ease: "power1.inOut",
+            scrollTrigger: {
+                trigger: ".timeline",
+                start: "top center",
+                end: "70% center",
+                onUpdate: (self) => {
+                    gsap.to(".timeline", {
+                        scaleY: 1 - self.progress,
+                    });
+                },
+            },
+        });
+
+        gsap.utils.toArray(".expText").forEach((text) => {
+            gsap.from(text, {
+                opacity: 0,
+                xPercent: 0,
+                duration: 1,
+                ease: "power2.inOut",
+                scrollTrigger: {
+                    trigger: text,
+                    start: "top 60%",
+                },
+            });
+        }, "<");
+    }, []);
+
     return <>
-        <section id="experience" className="w-full md:mt-40 mt:20 section-padding xl:px-0">
+        <section id="experience" className="w-full md:mt-40 mt:20 section-padding xl:px-0 overflow-hidden">
             <div className="w-full h-full md:px-20 px-5">
                 <TitleHeader
                     title="Professional Work Experience"
@@ -19,6 +69,35 @@ export default function ExperienceSection() {
                                             <img src={card.imgPath} alt={card.title} />
                                         </div>
                                     </GlowCard>
+                                </div>
+                                <div className="xl:w-4/6">
+                                    <div className="flex items-start">
+                                        <div className="timeline-wrapper">
+                                            <div className="timeline" />
+                                            <div className="gradient-line w-1 h-full" />
+                                        </div>
+                                        <div className="exp-text flex xl:gap-20 md:gap-10 gap-5 relative z-20">
+                                            <div className="timeline-logo">
+                                                <img src={card.logoPath} alt="logo" />
+                                            </div>
+                                            <div>
+                                                <h1 className="font-semibold text-3xl">{card.title}</h1>
+                                                <p className="my-5 text-white-50">
+                                                    🗓️ {card.date}
+                                                </p>
+                                                <p className="text-blue-50 italic">
+                                                    Responsibilities
+                                                </p>
+                                                <ul className="list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50">
+                                                    {card.responsibilities.map((responsibility) => (
+                                                        <li key={responsibility} className="text-lg">
+                                                            {responsibility}
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         ))}
